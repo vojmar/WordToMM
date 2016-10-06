5ab04ad0cef35acdc30f662a37b86fc128e90fef
DSroD <dsrod@seznam.cz>
2016-10-06 19:49:32 +0200

    Create FreeMindNodeCollection class for later use in data storing mechanisms

--- OdtToMm/OdtToMm/FreeMindNode.cs
+++ OdtToMm/OdtToMm/FreeMindNode.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace OdtToMm
 {
-    class FreeMindNode
+    public class FreeMindNode
     {
 
         static int lastId;
@@ -17,20 +18,118 @@ namespace OdtToMm
         public string text { get; private set; }
         private bool topNode;
 
-        FreeMindNode()
+        public FreeMindNode()
         {
             lastId = 0;
             this.id = lastId;
             topNode = true;
         }
         
-        FreeMindNode(int parentId, string text)
+        public FreeMindNode(int parentId, string text)
         {
             lastId++;
             this.id = lastId;
             this.parentId = parentId;
             this.text = text;
             this.topNode = false;
+        }
+    }
+
+    public class FreeMindNodeCollection : ICollection
+    {
+        private object syncRoot;
+        private FreeMindNode[] col;
+
+        public FreeMindNodeCollection()
+        {
+            this.col = new FreeMindNode[0];
+        }
+
+        public FreeMindNodeCollection(FreeMindNode[] nodeArray)
+        {
+            this.col = nodeArray;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.col.Length;
+            }
+        }
+
+        public bool IsSynchronized
+        {
+            get { return (this.syncRoot != null) ? true : false; }
+        }
+
+        public object SyncRoot
+        {
+            get
+            {
+                return syncRoot;
+            }
+        }
+
+        bool ICollection.IsSynchronized
+        {
+            get
+            {
+                return IsSynchronized;
+            }
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+            // TODO: Implement
+        }
+
+        public FreeMindNodeEnum GetEnumerator()
+        {
+            return new FreeMindNodeEnum(col);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(FreeMindNode node)
+        {
+            throw new NotImplementedException();
+            //TODO: Implement
+        }
+    }
+
+    public class FreeMindNodeEnum : IEnumerator
+    {
+
+        private FreeMindNode[] col;
+        int index = -1;
+
+        public FreeMindNodeEnum(FreeMindNode[] nodeCol)
+        {
+            this.col = nodeCol;
+        }
+
+        public object Current
+        {
+            get
+            {
+                return col[index];
+            }
+        }
+
+        public bool MoveNext()
+        {
+            index++;
+            return (index < col.Length) ? true : false;
+        }
+
+        public void Reset()
+        {
+            index = -1;
         }
     }
 }
