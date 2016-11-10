fa6de7545717f6480cc82006ccd8be2247b1a5dc
EbrithilNogare <davidnapravnik@gmail.com>
2016-11-10 16:10:44 +0100

    to sami co pred chvili

--- OdtToMm/OdtToMm/Form1.cs
+++ OdtToMm/OdtToMm/Form1.cs
@@ -21,6 +21,7 @@ namespace OdtToMm
             InitializeComponent();
             ODTofd = new OpenFileDialog();
             ODTofd.Filter = "odt files |*.odt|All files (*.*)|*.*";
+            ODTofd.Multiselect = false;
             MMsfd = new SaveFileDialog();
         }
 
